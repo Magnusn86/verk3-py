61b50b8e72de4e58b7cc14f4b2b4bfac921b40a0
Magnusn86 <mn@simnet.is>
2016-12-10 13:19:32 +0000

    búinn að græja uppsetninguna á dict fyrir episodes og movies

--- clean.py
+++ clean.py
@@ -15,12 +15,29 @@ def clean(path, dest):
         if f.endswith(".avi") or f.endswith('.mp4'):
             print (f)
     '''
+    # e = { 'episode title' : { 'season': [ [ episode name, episode path] ]
+    epFullDetails = {}
+    # e = { 'episode title' : { 'episode number': [ [ episode path] ]
+    epNoSeason = {}
+    # m = { 'movie title' : [ [ 'title', path ] ] } - pæling að bæta við ári
+    m = {}
     s = set()
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(".avi") or file.endswith('.mp4') or file.endswith('.mkv') or file.endswith('.mpeg4') or file.endswith('.m4v'):
                 f = os.path.join(root, file)
-                print (guessit(f))
+                print(f)
+                d = guessit(f)
+                print(d)
+                print(d['type'])
+                if d['type'] == 'movie':
+                    m.setdefault((d['title']).title(), []).append([ d['year'], f])
+                elif d['type'] == 'episode':
+                    if not d.get('season', None) == None:
+                        epFullDetails.setdefault((d['title']).title(), {}).setdefault(d['season'], set()).add((d['episode'], f))
+                    else:
+                        epNoSeason.setdefault((d['title']).title(), {}).setdefault(d['episode'], set()).add(f)
+
                 '''if not re.search('((s|S)[0-9]+(e|E)[0-9]+)', f):
                     print()
                     print(os.path.join(root, file))'''
@@ -32,10 +49,6 @@ def clean(path, dest):
                     for i in g:
                         print (i)'''
 
-                #for i in range(len(f)):
-                 #   if f[len(f)-i-1] == '.' :
-                  #    s.add(f[:len(f)-i-1])
-
                 #print(dest)
                 #shutil.move(f, dest)
                 #with open(os.path.join(root, file), 'r') as f:
@@ -43,9 +56,18 @@ def clean(path, dest):
                         #print(l)
                         #shutil.move(l, dest+ '/'+ path)
 
+    for i in epNoSeason:
+        print (i)
+        print (epNoSeason[i])
+
+    for i in epFullDetails:
+        print (i)
+        print(epFullDetails[i])
+
+    print (m)
     print('s: ')
     for i in s:
         print(i)
 
 
-print (clean('downloads','downloads1'))
+print (clean('test','downloads1'))
