7ff430e481ce6fb2c635ce593fa3e55aa8c52653
tumi23 <tumi435@gmail.com>
2016-12-11 15:07:42 +0000

    Breytti copy í move og bætti við checkum hvort fællinn sé nú þegar til á nýja staðnum og að hann er til svo hægt er að færa hann

--- clean.py
+++ clean.py
@@ -3,8 +3,8 @@ import shutil
 import sys
 from guessit import guessit
 
-path = sys.argv[1]
-dest = sys.argv[2]
+path = 'downloads'
+dest = 'TestFolder'
 
 # e = { 'episode title' : { 'season': [ [ episode name, episode path] ]
 epFullDetails = {}
@@ -18,7 +18,7 @@ s = set()
 print('Guessing titles, episode names etc....')
 for root, dirs, files in os.walk(path):
     for file in files:
-        if file.endswith(".avi") or file.endswith('.mp4') or file.endswith('.mkv') or file.endswith('.mpeg4') or file.endswith('.m4v'):
+        if file.endswith(".avi") or file.endswith('.mp4') or file.endswith('.mkv') or file.endswith('.mpeg4') or file.endswith('.m4v') or file.endswith('mp3'):
             f = os.path.join(root, file)
             d = guessit(f)
             if d['type'] == 'movie':
@@ -45,14 +45,20 @@ for i in epNoEpisode:
     for k in epNoEpisode[i]:
         if not os.path.exists(dest + '/Undefined From ' + k):
             os.makedirs(dest + '/Undefined From ' + k)
-        shutil.copy(k, dest + '/Undefined From ' + k)
+        temp = k.split('/')
+        temp = dest + '/Undefined From ' + k + temp[len(temp)-1]
+        if not os.path.isfile(temp) and os.path.isfile(k):
+            shutil.move(k, dest + '/Undefined From ' + k)
 
 for i in epNoSeason:
     if not os.path.exists(episodePath+'/'+i):
         os.makedirs(episodePath+'/' + i + '/Undefined')
     for k in epNoSeason[i]:
         for s in epNoSeason[i][k]:
-            shutil.copy(s, episodePath+'/' + i + '/Undefined')
+            temp = s.split('/')
+            temp = episodePath+'/' + i + '/Undefined' + '/' + temp[len(temp)-1]
+            if not os.path.isfile(temp) and os.path.isfile(s):
+                shutil.move(s, episodePath+'/' + i + '/Undefined')
 
 for i in epFullDetails:
     if not os.path.exists(episodePath+'/'+i):
@@ -61,10 +67,17 @@ for i in epFullDetails:
         if not os.path.exists(episodePath+'/'+i+'/Season '+ str(k)):
             os.makedirs(episodePath+'/'+i+'/Season '+ str(k))
         for s in epFullDetails[i][k]:
-            shutil.copy(s[1], episodePath+'/'+i+'/Season ' + str(k))
+            temp = s[1].split('/')
+            temp = episodePath+'/'+i+'/Season ' + str(k) + '/' + temp[len(temp)-1]
+            if not os.path.isfile(temp) and os.path.isfile(s[1]):
+                shutil.move(s[1], episodePath+'/'+i+'/Season ' + str(k))
 
 for i in m:
     if not os.path.exists(moviePath+'/'+i):
         os.makedirs(moviePath+'/'+i)
-    shutil.copy(m[i][0], moviePath+'/'+i)
+    temp = m[i][0].split('/')
+    temp = moviePath + '/' + i + '/' + temp[len(temp)-1]
+    if not os.path.isfile(temp) and os.path.isfile(m[i][0]):
+        shutil.move(m[i][0], moviePath + '/' + i)
+
 print('All files moved to '+ dest +', movies are in Movies folder, shows in Shows folder and files we couldnt guess are in the Undefined folder.')
