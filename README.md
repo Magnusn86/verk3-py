5ed0e86bf16fdaca03b2b9e2146639db9822c4a8
Magnusn86 <mn@simnet.is>
2016-12-11 21:31:07 +0000

    working copy of verk4

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# verk3-py
+# PRLA - Forritunarmálið Python
+# Verkefni 4
+
+###Nemendur: Magnús Norðdahl og Tumi Guðmundsson
+
+####Implementation
+Við notum guessit module úr PyPI
+
+Tökum saman:
+
+Allar þáttaraðir og setjum þær í dest/TV Shows/Heiti á þáttaröð/Season X/Heiti á skránni
+
+Allar bíómyndir og setjum þær í dest/Movies/Heitið á myndinni
+
+Alla tónlist og setjum hana í dest/Music/Nafnið á skránni
+
+Reglur:
+Við gerðum ráð fyrir á í viðkomandi folder sé bara til þess að geyma kvikmyndir, bíómyndir og tónlist. Ef eitthvað annað er í folder-num þá er því eytt  
+
+Ef skráin er þegar til í dest folder þá eyðum við skránni úr path folder
+
+Ef skrá sem var færð var í folder er akkúrat þeim folder eytt Það þýðir þó ekki að allar
--- clean.py
+++ clean.py
@@ -3,97 +3,158 @@ import shutil
 import sys
 import re
 from guessit import guessit
+import datetime
 
-path = 'downloads'
+path = 'downloads 8'
 dest = 'TestFolder'
 
-# e = { 'episode title' : { 'season': [ [ episode name, episode path] ]
+
+if not os.path.isdir(path):
+    print ('Path ' + path + ' is not an existing path on this computer')
+    exit()
+
+# epFullDetails = { 'episode title' : { 'season': [ [ episode name, episode path] ]
 epFullDetails = {}
-# e = { 'episode title' : { 'episode number': [ [ episode path] ]
+
+# epNoSeason = { 'episode title' : [ [ episode path] ]
 epNoSeason = {}
-# e = { 'episode title' : { 'season': [ [ episode path]
-epNoEpisode = {}
-# m = { 'movie title' : [ [ 'title', path ] ] } -
-m = {}
-s = set()
+
+#epUndefined = [ path , path ]
+epUndefined = []
+
+# movies = { 'movie title' : [ [ path, year ]  }
+movies = {}
+
+# music = [ path ]
+music = []
+
+#Name of all TV Shows found
 episodesFound = set()
-delete = []
+
+#Name of all Movies found
+moviesFound = set()
+
+#All undefined titled movies
+moviesUndefined = []
+
+#Folders to remove
+removeFolder = set()
+
+#otherFiles that are dont match what we are looking for
 otherFiles = []
+
+#path to files to delete
+deleteFiles = []
+
+#TODO prompt user for if he wants movies sorted after year
+
 print('Guessing titles, episode names etc....')
 for root, dirs, files in os.walk(path):
+
     for file in files:
         f = os.path.join(root, file)
+
         if re.search('((s|S)(a|A)(m|M)(p|P)(l|L)(e|E))', file):
-            delete.append(f)
-            break
-        elif file.endswith(".avi") or file.endswith('.mp4') or file.endswith('.mkv') or file.endswith('.mpeg4') or file.endswith('.m4v') or file.endswith('.srt') or file.endswith('.sub') or file.endswith('.sbv') or file.endswith('mp3'):
+            os.remove(os.path.join(root, file))
+            removeFolder.add(root)
+
+        elif file.endswith((".avi", '.mp4', '.mkv', '.mpeg4', '.m4v', '.mov', '.flv', '.wmv', '.mpg', '.mpeg', '.srt', '.sub', '.sbv')):
+
             f = os.path.join(root, file)
-            #print (f)
             d = guessit(f)
-            #print(d)
+
             if d['type'] == 'movie':
-                m.setdefault((d['title']).title(), []).append(f)
-            elif d['type'] == 'episode':
-                if 'episode' in d:
-                    if not d.get('season', None) == None:
-                        epFullDetails.setdefault((d['title']).title(), {}).setdefault(d['season'], set()).add((d['episode'], f))
+                if not d.get('title', None) == None:
+
+                    if d.get('year', None) == None:
+                        year = 'N/A'
                     else:
-                        epNoSeason.setdefault((d['title']).title(), {}).setdefault(d['episode'], set()).add(f)
+                        year = d['year']
+
+                    movies.setdefault((d['title']).title(), []).append([f, year])
+                    moviesFound.add(d['title'])
+                else:
+                    moviesUndefined.append((f))
+                removeFolder.add(root)
+
+            elif d['type'] == 'episode':
+                if 'title' in d:
+                    if not d.get('season', None) == None:
+                        epFullDetails.setdefault((d['title']).title(), {}).setdefault(d['season'], []).append((f))
+                    else:
+                        epNoSeason.setdefault((d['title']).title(), []).append(f)
+                    episodesFound.add((d['title']).title())
+                    removeFolder.add(root)
                 else:
                     if not file.startswith('.'):
-                        epNoEpisode.setdefault((d['title']).title(), set()).add(f)
+                        epUndefined.append(f)
+                        removeFolder.add(root)
+        elif file.endswith('mp3'):
+            music.append(f)
+            removeFolder.add(root)
         else:
             otherFiles.append(f)
 
-#print('List of all episodes Found')
-'''for i in epFullDetails:
-    print(i)
-for i in epNoSeason:
-    print(i)
-for i in epNoEpisode:
-    print(i)'''
+#Print out all found TV shows and episodes
+print('\nList of all TV shows found in the "/' + path + '" directory')
+if len(epFullDetails) > 0 or len(epNoSeason) > 0:
+    for i in episodesFound:
+        print(i)
+        for j in epFullDetails.get(i, []):
+            print('\tSeason ' + str(j))
+            for k in epFullDetails[i][j]:
+                print('\t\t'+ str(k.split('/')[-1]))
+
+        if not len(epNoSeason.get(i, [])) == 0:
+            print('\tUndefined')
+        for j in epNoSeason.get(i, []):
+            print('\t\t' + str(j.split('/')[-1]))
+
+    #TODO prompt user for correcting name of TV Show
+else:
+    print('None')
+
+if len(movies) > 0:
+    print('\n\nList of all Movies found in ' + path + ' directory')
+    for i in movies:
+        for j in movies[i]:
+            print('\t' +i)
+
+    #TODO prompt user for changing names of Movies or if wants to put movies in folders sorted after year
+
+if len(music) > 0:
+    print('\n\nList of all Movies found in ' + path + ' directory')
+    for i in music:
+        print('\t' + i.split('/')[-1])
 
 
-'''print ()
-print('other files')
-for i in otherFiles:
-    print(i)
 
-print ()
-print('delete files')
-for i in delete:
-    print(i)'''
 
-#print (m)
-
-print('Moving files to nicely structured folders in ' + dest)
+#Moveing all files to their structured directories
+print('\nMoving files to nicely structured folders in ' + dest)
 if not os.path.exists(dest):
     os.makedirs(dest)
-episodePath = dest + '/Shows'
+episodePath = dest + '/TV Shows'
 moviePath = dest + '/Movies'
+musicPath = dest + '/Music'
 if not os.path.exists(episodePath):
         os.makedirs(episodePath)
 if not os.path.exists(moviePath):
         os.makedirs(moviePath)
 
-for i in epNoEpisode:
-    for k in epNoEpisode[i]:
-        if not os.path.exists(dest + '/Undefined From ' + k):
-            os.makedirs(dest + '/Undefined From ' + k)
-        temp = k.split('/')
-        temp = dest + '/Undefined From ' + k + temp[len(temp)-1]
-        if not os.path.isfile(temp) and os.path.isfile(k):
-            shutil.move(k, dest + '/Undefined From ' + k)
-
 for i in epNoSeason:
     if not os.path.exists(episodePath+'/'+i):
         os.makedirs(episodePath+'/' + i + '/Undefined')
-    for k in epNoSeason[i]:
-        for s in epNoSeason[i][k]:
-            temp = s.split('/')
-            temp = episodePath+'/' + i + '/Undefined' + '/' + temp[len(temp)-1]
-            if not os.path.isfile(temp) and os.path.isfile(s):
+    for s in epNoSeason[i]:
+        temp = s.split('/')
+        temp = episodePath+'/' + i + '/Undefined' + '/' + temp[len(temp)-1]
+        if not os.path.isfile(temp) and os.path.isfile(s):
+            try:
                 shutil.move(s, episodePath+'/' + i + '/Undefined')
+            except Exception as e:
+                print(e)
+        else:
+            os.remove(s)
 
 for i in epFullDetails:
     if not os.path.exists(episodePath+'/'+i):
@@ -102,21 +163,100 @@ for i in epFullDetails:
         if not os.path.exists(episodePath+'/'+i+'/Season '+ str(k)):
             os.makedirs(episodePath+'/'+i+'/Season '+ str(k))
         for s in epFullDetails[i][k]:
-            temp = s[1].split('/')
+            temp = s.split('/')
             temp = episodePath+'/'+i+'/Season ' + str(k) + '/' + temp[len(temp)-1]
-            if not os.path.isfile(temp) and os.path.isfile(s[1]):
-                shutil.move(s[1], episodePath+'/'+i+'/Season ' + str(k))
+            if not os.path.isfile(temp) and os.path.isfile(s):
+                try:
+                    shutil.move(s, episodePath+'/'+i+'/Season ' + str(k))
+                except Exception as e:
+                    print(e)
+            else:
+                os.remove(s)
 
-for i in m:
+for i in movies:
     if not os.path.exists(moviePath+'/'+i):
         os.makedirs(moviePath+'/'+i)
-    temp = m[i][0].split('/')
-    temp = moviePath + '/' + i + '/' + temp[len(temp)-1]
-    if not os.path.isfile(temp) and os.path.isfile(m[i][0]):
-        shutil.move(m[i][0], moviePath + '/' + i)
+    for j in movies[i]:
+        temp = j[0].split('/')
+        temp = moviePath + '/' + i + '/' + temp[len(temp)-1]
+        if not os.path.isfile(temp) and os.path.isfile(j[0]):
+            try:
+                shutil.move(j[0], moviePath + '/' + i)
+            except Exception as e:
+                print(e)
+        else:
+            os.remove(j[0])
 
-print('All files moved to '+ dest +', movies are in Movies folder, shows in Shows folder and files we couldnt guess are in the Undefined folder.')
+for i in music:
+    if not os.path.exists(musicPath +'/'+i):
+        os.makedirs(musicPath +'/'+i)
+    temp = i.split('/')
+    temp = musicPath + '/' + i + '/' + temp[len(temp)-1]
+    if not os.path.isfile(temp) and os.path.isfile(i):
+        try:
+            shutil.move(i, musicPath + '/' + i)
+        except Exception as e:
+            print(e)
+    else:
+        os.remove(i)
 
 
 
-print (clean('test', 'downloads1'))
+
+
+print('\nAll files moved to '+ dest +', movies are in Movies folder, shows in Shows folder and files we couldnt guess are in the Undefined folder.')
+
+
+
+#Write out the episodes added to a file to see what was migrated
+
+date = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+filename = dest + '/Import at ' + date
+
+file = open(filename, "w")
+
+file.write('Import log from ' + date + '\n')
+
+file.write('\nEpisodes added:' + '\n')
+if len(episodesFound) == 0:
+    file.write('None' + '\n')
+else:
+    for i in episodesFound:
+        file.write(i + '\n')
+        for j in epFullDetails.get(i, []):
+            file.write('\tSeason ' + str(j))
+            for k in epFullDetails[i][j]:
+                file.write('\t\t'+ str(k.split('/')[-1]))
+
+        if not len(epNoSeason.get(i, [])) == 0:
+            file.write('\tUndefined')
+        for j in epNoSeason.get(i, []):
+            file.write('\t\t' + str(j.split('/')[-1]))
+
+file.write('\nMovies added:' + '\n')
+if len(movies) == 0:
+    file.write('None' + '\n')
+else:
+    for i in movies:
+        file.write(i + '\n')
+
+file.write('\nMusic added:' + '\n')
+if len(music) == 0:
+    file.write('\t' + 'None' + '\n')
+else:
+    for i in music:
+        file.write('\t' + i.split('/')[-1] + '\n')
+
+file.close()
+
+
+#Remove all the files that are not needed
+for i in otherFiles:
+    os.remove(i)
+
+
+for i in removeFolder:
+    try:
+        os.removedirs(i)
+    except:
+        var = 'Do nothing'
